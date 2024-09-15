2446228b418ca2d28a691eff8d75a68a4048740d
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-09-15 20:22:57 +0200

    maj img du slider dans work service

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nom du cache
-const CACHE_NAME = "nina-carducci-cache-v5";
+const CACHE_NAME = "nina-carducci-cache-v12";
 
 // Liste des ressources à mettre en cache (y compris les nouvelles images)
 const urlsToCache = [
@@ -7,6 +7,7 @@ const urlsToCache = [
   "./assets/style.min.css",
   "./assets/scripts.min.js",
   "./assets/bootstrap/bootstrap.bundle.min.js",
+
   // Images du slider
   "./assets/images/slider/nina-Carducci-entreprise-homme-se-rendant-au-bureau-W320.webp",
   "./assets/images/slider/nina-Carducci-entreprise-homme-se-rendant-au-bureau-W650.webp",
@@ -20,46 +21,6 @@ const urlsToCache = [
   "./assets/images/slider/nina-Carducci-portrait-homme-spectateur-concert-W650.webp",
   "./assets/images/slider/nina-Carducci-portrait-homme-spectateur-concert-W1000.webp",
   "./assets/images/slider/nina-Carducci-portrait-homme-spectateur-concert-W1920.webp",
-  // Images gallery
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-spectacle-bordeaux-W650.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-spectacle-bordeaux-W320.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-spectacle-bordeaux-SQ650.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-spectacle-bordeaux-SQ320.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-concert-chanteur-W650.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-concert-chanteur-W320.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-concert-chanteur-SQ650.webp",
-  "./assets/images/gallery/concerts/nina-carducci-photographe-evenement-concert-chanteur-SQ320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-portrait-corporate-nouveau-directeur-W650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-portrait-corporate-nouveau-directeur-SQ650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-portrait-corporate-nouveau-directeur-W320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-portrait-corporate-nouveau-directeur-SQ320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-entreprise-vie-corporate-nouveaux-bureaux-portrait-presidente-W650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-entreprise-vie-corporate-nouveaux-bureaux-portrait-presidente-SQ650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-entreprise-vie-corporate-nouveaux-bureaux-portrait-presidente-SQ320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-entreprise-vie-corporate-nouveaux-bureaux-portrait-presidente-W320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-corporate-jeune-femme-au-bureaux-W650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-corporate-jeune-femme-au-bureaux-W320.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-corporate-jeune-femme-au-bureaux-SQ650.webp",
-  "./assets/images/gallery/entreprise/nina-carducci-photographe-corporate-jeune-femme-au-bureaux-SQ320.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-shooting-jeune-couple-marié-W650.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-shooting-jeune-couple-marié-SQ650.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-album-cover-mariage-SQ650.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-album-cover-mariage-W650.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-shooting-jeune-couple-marié-SQ320.webp",
-  "./assets/images/gallery/mariage/nina-carducci-photographe-album-cover-mariage-SQ320.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-femme-SQ320.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-femme-SQ650.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-femme-W650.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-homme-SQ320.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-homme-SQ650.webp",
-  "./assets/images/gallery/portraits/nina-carducci-photographe-shooting-portrait-homme-W650.webp",
-  // Images main
-  "./assets/images/main/appareil-photographe-SQ250.webp",
-  "./assets/images/main/appareil-photographe-SQ500.webp",
-  "./assets/images/main/appareil-photographe-SQ700.webp",
-  "./assets/images/main/nina-carducci-photographe-SQ250.webp",
-  "./assets/images/main/nina-carducci-photographe-SQ500.webp",
-  "./assets/images/main/nina-carducci-photographe-SQ700.webp",
 ];
 
 // Installation du Service Worker
